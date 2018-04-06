2549c589c58ec08511427f66693172cce8fd013f
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2018-04-06 16:05:05 +0200

    ⬆️ 🔊 Add logging

--- packages/image-proxy/src/handler.ts
+++ packages/image-proxy/src/handler.ts
@@ -15,29 +15,30 @@ const s3 = new S3();
 const ALLOWED_RESOLUTIONS = new Map([[185, 273], [216, 122]]); // [ width, height ]
 
 function fetchImage(logger: Logger, image: ImageInformation) {
+  const log = (msg: string) => logger.log(msg);
   if (image.type === 'episode') {
-    logger.captureBreadcrumb({ message: `Fetch episode`, category: 'debug', data: undefined });
-    return theTvDb.fetchEpisodeImage(image.id);
+    logger.log(`Fetch episode`);
+    return theTvDb.fetchEpisodeImage(image.id, log);
   } else if (image.type === 'poster') {
-    logger.captureBreadcrumb({ message: `Fetch poster`, category: 'debug', data: undefined });
-    return theTvDb.fetchShowPoster(image.id);
+    logger.log(`Fetch poster`);
+    return theTvDb.fetchShowPoster(image.id, log);
   } else if (image.type === 'fanart') {
-    logger.captureBreadcrumb({ message: `Fetch fanart`, category: 'debug', data: undefined });
-    return theTvDb.fetchShowFanart(image.id);
+    logger.log(`Fetch fanart`);
+    return theTvDb.fetchShowFanart(image.id, log);
   } else {
-    logger.captureBreadcrumb({ message: `Unknown type`, category: 'debug', data: undefined });
+    logger.log(`Unknown type`);
     throw new BadRequest();
   }
 }
 
 function resizeImage(logger: Logger, buffer: Buffer, width?: number, height?: number): Promise<Buffer> {
   if (width && height) {
-    logger.captureBreadcrumb({ message: 'Resize Image', category: 'debug', data: { width, height } });
+    logger.log('Resize Image');
     return sharp(buffer)
       .resize(width, height)
       .toBuffer();
   }
-  logger.captureBreadcrumb({ message: 'Will not resize Image', category: 'debug', data: { width, height } });
+  logger.log('Will not resize Image');
   return Promise.resolve(buffer);
 }
 
@@ -66,17 +67,14 @@ function fetchAndSave(logger: Logger, image: ImageInformation) {
 export function imageFetcher(event: APIGatewayEvent, logger: Logger) {
   const key = event.queryStringParameters.key;
   logger.log('Requesting: ' + key);
-  logger.captureBreadcrumb({ message: 'Requesting: ' + key, category: 'debug', data: undefined });
   const image = imageInformation(key);
 
   if (!image) {
-    logger.captureBreadcrumb({ message: `${key} is not a valid key`, category: 'debug', data: undefined });
     logger.log(`${key} is not a valid key`);
     return Promise.resolve(BAD_REQUEST_RESPONSE);
   }
 
   if (image.width && !ALLOWED_RESOLUTIONS.has(image.width)) {
-    logger.captureBreadcrumb({ message: `${image.width} is not a valid width`, category: 'debug', data: undefined });
     logger.log(`${image.width} is not a valid width`);
     return Promise.resolve(BAD_REQUEST_RESPONSE);
   }
@@ -84,6 +82,7 @@ export function imageFetcher(event: APIGatewayEvent, logger: Logger) {
   return fetchAndSave(logger, image)
     .then(() => move(image.key))
     .catch(error => {
+      logger.log(error && error.message);
       if (error instanceof NotFound) {
         return NOT_FOUND_RESPONSE;
       } else if (error instanceof BadRequest) {
