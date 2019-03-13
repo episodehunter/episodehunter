29e901ebefe05588c41ffa5d371d9169fd31b440
Tjoskar <mr.karlsson.oskar@gmail.com>
2019-03-13 12:24:29 +0100

    ✨

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -1,123 +1,100 @@
 import { Context } from 'aws-lambda';
 import * as DnaLogger from 'logdna';
-import * as raven from 'raven';
+import * as Sentry from '@sentry/node';
 
 interface DnaLogger {
-  log: (message: string, level: 'Debug' | 'Trace' | 'Info' | 'Warn' | 'Error' | 'Fatal') => void;
+  log: (
+    message: string,
+    opt: { level: 'Debug' | 'Trace' | 'Info' | 'Warn' | 'Error' | 'Fatal'; meta: any }
+  ) => void;
 }
 
-export class Logger {
-  private awsContext: Context;
-  private ravenDns: string;
-  private logDnaApiKey: string;
-  private ravenProjectId: string;
-  private dnaLogger: DnaLogger;
-  private dnaLoggerInstance: any;
+export function setupLogger(ravenDns: string, logDnaApiKey: string) {
+  installSentry(ravenDns);
+  const dnaLogger = installLogdna(logDnaApiKey);
+  return createCreateSentryLogger(dnaLogger);
+}
 
-  constructor(ravenDns: string, ravenProjectId: string, logDnaApiKey: string) {
-    this.ravenDns = ravenDns;
-    this.ravenProjectId = ravenProjectId;
-    this.logDnaApiKey = logDnaApiKey;
-  }
-
-  install(awsContext: Context) {
-    this.awsContext = awsContext;
-    this.installRaven();
-    this.installLogdna();
-  }
-
-  info(...args: any[]) {
-    console.info(...args);
-    this.captureBreadcrumb({
-      category: 'info-log',
-      message: args.join(', ')
-    });
-    this.dnaLogger.log(args.join(', '), 'Info');
-  }
-
-  log(...args: any[]) {
-    console.log(...args);
-    this.captureBreadcrumb({
-      category: 'log-log',
-      message: args.join(', ')
-    });
-    this.dnaLogger.log(args.join(', '), 'Info');
-  }
-
-  warn(...args: any[]) {
-    console.warn(...args);
-    this.captureBreadcrumb({
-      category: 'warn-log',
-      message: args.join(', ')
-    });
-    this.dnaLogger.log(args.join(', '), 'Warn');
-  }
-
-  captureBreadcrumb(breadcrumb: { message: string; category: string; data?: object }) {
-    raven.captureBreadcrumb(breadcrumb);
-  }
-
-  captureException(error: Error) {
-    raven.captureException(error);
-    this.dnaLogger.log(String(error), 'Fatal');
-    DnaLogger.flushAll(() => null);
-  }
-
-  captureMessage(msg: string) {
-    raven.captureMessage(msg);
-  }
-
-  eventStart(message: string, category = 'event') {
-    const startMsg = `[START] ${message}. Remaning time ${this.awsContext.getRemainingTimeInMillis()}ms`;
-    console.log(startMsg);
-    this.captureBreadcrumb({
-      category: 'Start ' + category,
-      message: startMsg
-    });
-    const startTime = process.hrtime();
-    return () => {
-      const endTime = process.hrtime(startTime);
-      const totalTime = endTime[0] * 1000 + endTime[1] / 1000000;
-      const endMsg = `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${this.awsContext.getRemainingTimeInMillis()}ms`;
-      console.log(endMsg);
-      this.captureBreadcrumb({
-        category: 'End ' + category,
-        message: `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${this.awsContext.getRemainingTimeInMillis()}ms`
+function createCreateSentryLogger(logdna: DnaLogger) {
+  return (context: Context, requestStack?: string[]) => {
+    const meta = {
+      functionName: context.functionName,
+      functionVersion: context.functionVersion,
+      requestId: context.awsRequestId,
+      logGroupName: context.logGroupName,
+      logStreamName: context.logStreamName,
+      requestStack
+    };
+    const log = (message: string) => {
+      console.log(message);
+      Sentry.addBreadcrumb({
+        level: Sentry.Severity.Log,
+        message,
+        data: meta
       });
+      logdna.log(message, { level: 'Info', meta });
     };
-  }
-
-  private installLogdna() {
-    this.dnaLoggerInstance = DnaLogger.setupDefaultLogger(this.logDnaApiKey, {
-      app: this.awsContext.functionName,
-      env: process.env.NODE_ENV,
-      index_meta: true
-    });
-    this.dnaLogger = {
-      log: (message: string, level: string) => {
-        this.dnaLoggerInstance.log(message, {
-          level,
-          meta: { functionName: this.awsContext.functionName, requestId: this.awsContext.awsRequestId }
-        });
+    const warn = (message: string) => {
+      console.warn(message);
+      Sentry.addBreadcrumb({
+        level: Sentry.Severity.Warning,
+        message,
+        data: meta
+      });
+      logdna.log(message, { level: 'Warn', meta });
+    };
+    const captureBreadcrumb = (message: string, category: string, data?: object) => {
+      console.log(message, category, data);
+      Sentry.addBreadcrumb({
+        level: Sentry.Severity.Warning,
+        message,
+        category,
+        data: Object.assign({}, meta, data)
+      });
+      logdna.log(message, { level: 'Error', meta });
+    };
+    const captureException = (error: Error & { awsRequestId?: string }) => {
+      error.awsRequestId = context.awsRequestId;
+      console.log(error);
+      Sentry.captureException(error);
+      try {
+        const logmsg = error.message + ' at ' + error.stack;
+        logdna.log(logmsg, { level: 'Fatal', meta });
+      } catch (error) {
+        logdna.log('Could not parse error message', { level: 'Fatal', meta });
       }
+      DnaLogger.flushAll();
     };
-  }
+    const eventStart = (message: string, category = 'event') => {
+      const startMsg = `[START] ${message}. Remaning time ${context.getRemainingTimeInMillis()}ms`;
+      captureBreadcrumb(startMsg, 'Start ' + category);
+      const startTime = process.hrtime();
+      return () => {
+        const endTime = process.hrtime(startTime);
+        const totalTime = endTime[0] * 1000 + endTime[1] / 1000000;
+        const endMsg = `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${context.getRemainingTimeInMillis()}ms`;
+        console.log(endMsg);
+        captureBreadcrumb(
+          `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${context.getRemainingTimeInMillis()}ms`,
+          'End ' + category
+        );
+      };
+    };
 
-  private installRaven() {
-    raven
-      .config(`https://${this.ravenDns}@sentry.io/${this.ravenProjectId}`, {
-        extra: {
-          functionName: this.awsContext.functionName,
-          functionVersion: this.awsContext.functionVersion,
-          requestId: this.awsContext.awsRequestId,
-          logGroupName: this.awsContext.logGroupName,
-          logStreamName: this.awsContext.logStreamName
-        },
-        autoBreadcrumbs: {
-          console: false,
-          http: true
-        }
-      })
-      .install();
-  }
+    return { log, warn, captureBreadcrumb, captureException, eventStart };
+  };
+}
+
+function installSentry(ravenDns: string) {
+  Sentry.init({
+    dns: ravenDns,
+    environment: process.env.NODE_ENV === 'develop' ? 'develop' : 'production'
+  });
+}
+
+function installLogdna(logDnaApiKey: string) {
+  return DnaLogger.setupDefaultLogger(logDnaApiKey, {
+    env: process.env.NODE_ENV === 'develop' ? 'develop' : 'production',
+    index_meta: true
+  });
 }
--- packages/logger/readme.md
+++ packages/logger/readme.md
@@ -1,3 +1,15 @@
 ## Logger for episodehunter
 
 A simple wrapper for the sentry and logdna clients.
+
+```ts
+import { setupLogger } from '@episodehunter/logger';
+
+const createLogger = setupLogger('ravenDns', 'logDnaApiKey');
+
+const handler = (event, context, cb) => {
+  const requestStack = ['id2', 'id1', 'id0'];
+  const logger = createLogger(context, requestStack);
+  logger.log('Hello!');
+};
+```
