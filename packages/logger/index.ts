e321537858bcfeb0b36a05104c38f0e7c4baa460
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2019-03-18 12:51:17 +0100

    Make it possible to use a fake logger

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -9,14 +9,29 @@ interface DnaLogger {
   ) => void;
 }
 
-export function setupLogger(ravenDns: string, logDnaApiKey: string) {
+export interface Logger {
+  log(message: string): void;
+  warn(message: string): void;
+  captureBreadcrumb(message: string, category: string, data?: object): void;
+  captureException(error: Error & { awsRequestId?: string }): void;
+  eventStart(message: string, category?: string): void;
+}
+
+export function setupLogger(
+  ravenDns?: string,
+  logDnaApiKey?: string
+): (context: Context, requestStack?: string[]) => Logger {
+  if (!ravenDns || !logDnaApiKey) {
+    console.warn('Logger: Missing ravenDns and/or logDnaApiKey, using console');
+    return createFakeLogger;
+  }
   installSentry(ravenDns);
   const dnaLogger = installLogdna(logDnaApiKey);
   return createCreateSentryLogger(dnaLogger);
 }
 
 function createCreateSentryLogger(logdna: DnaLogger) {
-  return (context: Context, requestStack: string[] = []) => {
+  return (context: Context, requestStack: string[] = []): Logger => {
     const meta = {
       functionName: context.functionName,
       functionVersion: context.functionVersion,
@@ -74,15 +89,55 @@ function createCreateSentryLogger(logdna: DnaLogger) {
         const totalTime = endTime[0] * 1000 + endTime[1] / 1000000;
         const endMsg = `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${context.getRemainingTimeInMillis()}ms`;
         console.log(endMsg);
-        captureBreadcrumb(
-          `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${context.getRemainingTimeInMillis()}ms`,
-          'End ' + category
-        );
+        captureBreadcrumb(endMsg, 'End ' + category);
       };
     };
 
     return { log, warn, captureBreadcrumb, captureException, eventStart };
   };
+}
+
+function createFakeLogger(context: Context, requestStack: string[] = []): Logger {
+  const meta = {
+    functionName: context.functionName,
+    functionVersion: context.functionVersion,
+    requestId: context.awsRequestId,
+    logGroupName: context.logGroupName,
+    logStreamName: context.logStreamName,
+    requestStack
+  };
+  const log = (message: string) => {
+    console.log(message);
+  };
+  const warn = (message: string) => {
+    console.warn(message);
+  };
+  const captureBreadcrumb = (message: string, category: string, data?: object) => {
+    console.log(message, category, data);
+  };
+  const captureException = (error: Error & { awsRequestId?: string }) => {
+    error.awsRequestId = context.awsRequestId;
+    console.log(error);
+    try {
+      const logmsg = error.message + ' at ' + error.stack;
+      console.log(logmsg, { level: 'Fatal', meta, app: context.functionName });
+    } catch (error) {
+      console.log('Could not parse error message', { level: 'Fatal', meta, app: context.functionName });
+    }
+  };
+  const eventStart = (message: string, category = 'event') => {
+    const startMsg = `[START] ${message}. Remaning time ${context.getRemainingTimeInMillis()}ms`;
+    console.log(startMsg, 'Start ' + category);
+    const startTime = process.hrtime();
+    return () => {
+      const endTime = process.hrtime(startTime);
+      const totalTime = endTime[0] * 1000 + endTime[1] / 1000000;
+      const endMsg = `[END] ${message}. Time elapsed: ${totalTime}ms. Remaning time ${context.getRemainingTimeInMillis()}ms`;
+      console.log(endMsg, 'End ' + category);
+    };
+  };
+
+  return { log, warn, captureBreadcrumb, captureException, eventStart };
 }
 
 function installSentry(ravenDns: string) {
