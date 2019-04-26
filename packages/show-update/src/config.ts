951b2aafe669857d264537c2540ce5ea53b59c96
tjoskar <kontakta@oskarkarlsson.nu>
2019-04-26 08:04:15 +0200

    🔥

--- packages/show-update/src/config.ts
+++ packages/show-update/src/config.ts
@@ -1,8 +1,6 @@
 interface Config {
   sentryDsn: string;
   logdnaKey: string;
-  dragonstoneApiKey: string;
-  dragonstoneUrl: string;
   redKeepApiKey: string;
   ehRedKeepUrl: string;
   theTvDbApiKey: string;
@@ -16,8 +14,6 @@ function createProdConfig(): Config {
   return {
     sentryDsn: process.env.AWS_SENTRY_DSN,
     logdnaKey: process.env.LOGDNA_KEY,
-    dragonstoneApiKey: process.env.DRAGONSTONE_API_KEY,
-    dragonstoneUrl: process.env.DRAGONSTONE_URL,
     redKeepApiKey: process.env.EH_RED_KEEP_API_KEY,
     ehRedKeepUrl: process.env.EH_RED_KEEP_URL,
     theTvDbApiKey: process.env.THE_TV_DB_API_KEY,
@@ -32,8 +28,6 @@ function createTestConfig() {
   return {
     sentryDsn: 'sentry-dsn',
     logdnaKey: 'LOGDNA_KEY',
-    dragonstoneApiKey: 'DRAGONSTONE_API_KEY',
-    dragonstoneUrl: 'DRAGONSTONE_URL',
     redKeepApiKey: 'EH_RED_KEEP_API_KEY',
     ehRedKeepUrl: 'EH_RED_KEEP_URL',
     theTvDbApiKey: 'THE_TV_DB_API_KEY',
