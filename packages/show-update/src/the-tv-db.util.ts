c1a3b634e139477fce04fd010ea770b8b92a0a05
tjoskar <kontakta@oskarkarlsson.nu>
2019-04-26 08:04:27 +0200

    🐛

--- packages/show-update/src/the-tv-db.util.ts
+++ packages/show-update/src/the-tv-db.util.ts
@@ -7,3 +7,5 @@ const theTvDb = new TheTvDb(config.theTvDbApiKey);
 export const fetchShow = (theTvDbId: number, logger: Logger) => theTvDb.fetchShow(theTvDbId, msg => logger.log(msg));
 export const fetchShowEpisodes = (theTvDbId: number, logger: Logger) =>
   theTvDb.fetchShowEpisodes(theTvDbId, undefined, msg => logger.log(msg));
+export const getInformationFromTvDb = (theTvDbId: number, logger: Logger) =>
+  Promise.all([fetchShow(theTvDbId, logger), fetchShowEpisodes(theTvDbId, logger)]);
