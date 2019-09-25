81d28270057a803cf3a938e3ae9a03c4784512f8
tjoskar <kontakta@oskarkarlsson.nu>
2019-09-25 15:54:12 +0200

    💚

--- packages/dragonstone/jest.config.js
+++ packages/dragonstone/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
-  collectCoverage: true
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/', '<rootDir>/coverage/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts']
 };
