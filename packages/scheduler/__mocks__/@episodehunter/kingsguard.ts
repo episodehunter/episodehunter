210d155f46184ae81f89934e8bd611b38d49373e
tjoskar <kontakta@oskarkarlsson.nu>
2020-02-24 17:22:12 +0100

    🐛

--- packages/scheduler/__mocks__/@episodehunter/kingsguard.ts
+++ packages/scheduler/__mocks__/@episodehunter/kingsguard.ts
@@ -2,7 +2,8 @@ export const createGuard = () => {
   return (fn: (event: any, logger: any, context: any) => Promise<any>) => (event: any, context: any) => {
     const logger = {
       log: () => {},
-      warn: console.warn
+      warn: console.warn,
+      track: () => Promise.resolve()
     };
     return fn(event, logger, context);
   };
